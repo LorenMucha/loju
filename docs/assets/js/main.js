eb098ea24977cd0b760ac0c99708700d75236c98
LorenMucha <lorenmucha@gmail.com>
2022-05-02 17:59:37 +0200

    fix docs

---
+++ docs/assets/js/main.js
@@ -0,0 +1,8 @@
+$(window).bind('mousewheel', function(event) {
+    if (event.originalEvent.wheelDelta >= 0) {
+        $('#navbar').show();
+    }
+    else {
+        $('#navbar').show();
+    }
+});
